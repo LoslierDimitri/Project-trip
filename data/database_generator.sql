0c1c970316946393bb0d71b1a612ecee146461d7
Vincent Ferigo <ferigo.vincent@orange.fr>
2022-07-05 14:28:45 +0200

    add database_generator.sql

---
+++ data/database_generator.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE `projet_trip` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE projet_trip;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  `prenom` varchar(45) NOT NULL,
+  `age` int NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SELECT * FROM users;
