d064a772abc93de4a92d6d10026f850404307fbd
LOSLIER Dimitri <loslierdimitri@gmail.com>
2022-07-26 16:05:50 +0200

    search to do

--- projet/back/function/result_search.php
+++ projet/back/function/result_search.php
@@ -14,9 +14,31 @@ function search($search_type, $voyage_region, $voyage_lieu_depart, $voyage_lieu_
     require ("result_format.php");
     require ("result_send.php");
 
+    require ("../../back/api/api_call.php");
+    require ("../../back/database/database_connect.php");
+    require ("../../back/database/database_disconnect.php");
+    require ("../../back/database/database_request.php");
+
     //recherche api
 
     //recherche bdd
+    if (isset($_SESSION['pseudo']) && isset($_SESSION["mot_de_passe"])) {
+    $connection = database_connect();
+
+    $user_nom = database_select_where($connection, "nom", "pseudo", $_SESSION["pseudo"]);
+    $user_prenom = database_select_where($connection, "prenom", "pseudo", $_SESSION["pseudo"]);
+    $user_age = database_select_where($connection, "age", "pseudo", $_SESSION["pseudo"]);
+    $user_sexe = database_select_where($connection, "sexe", "pseudo", $_SESSION["pseudo"]);
+    $user_mail = database_select_where($connection, "email", "pseudo", $_SESSION["pseudo"]);
+
+    echo $user_nom[0]["nom"] . "<br>";
+    echo $user_prenom[0]["prenom"] . "<br>";
+    echo $user_age[0]["age"] . "<br>";
+    echo $user_sexe[0]["sexe"] . "<br>";
+    echo $user_mail[0]["email"] . "<br>";
+
+    $connection = database_disconnect();
+    }
 
     //affinage recherche
 
@@ -28,4 +50,3 @@ function search($search_type, $voyage_region, $voyage_lieu_depart, $voyage_lieu_
     //envoie des info sur le front
     result_send();
 }
-?>
--- projet/front/test/test_function_result.php
+++ projet/front/test/test_function_result.php
@@ -4,5 +4,9 @@ include ("test_include.php");
 
 <?php
 
-?>
+// require ("../../back/function/result_search.php");
 
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     search($_POST["type_search"], $_POST["voyage_region"], $_POST["voyage_lieu_depart"], $_POST["voyage_lieu_arrive"], $_POST["voyage_date_aller"], $_POST["voyage_date_retour"], $_POST["voyage_nombre_personne_adulte"], $_POST["voyage_nombre_personne_enfant"], $_POST["voyage_formule"]);
+// }
+?>
