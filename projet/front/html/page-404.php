46498b89c1bbb4e0eda8ea2b5844cc8e14ca0456
steeve <paturotsteeve@gmail.com>
2022-07-27 15:35:17 +0200

    création bouton finalisé

--- projet/front/html/page-404.php
+++ projet/front/html/page-404.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href=".projet/front/css/navbar.css">
-    <link rel="stylesheet" href=".projet/front/css/footer.css">
-    <link rel="stylesheet" href=".projet/front/css/page-404.css">
+    <link rel="stylesheet" href="./projet/front/css/navbar.css">
+    <link rel="stylesheet" href="./projet/front/css/footer.css">
+    <link rel="stylesheet" href="./projet/front/css/page-404.css">
     <title>erreur 404</title>
 </head>
 
@@ -28,7 +28,7 @@
   <div class="card-body">
     <h2 class="card-title">404</h2>
     <p class="card-text">Désolé, mais la page que vous avez demandé n'est pas disponible</p>
-    <a class="btnAccueil" href="./">Accueil</a>
+    <a class="btnAccueil" href="./"><span class="colorBtn">Accueil</span></a>
   </div>
 </div></div>
 
