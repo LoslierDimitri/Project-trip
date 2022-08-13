58dcc5f47c2691c25539c10dcb44f801f1081b65
Damien L <damien.laneelle33@gmail.com>
2022-08-13 15:32:31 +0200

    Picto-add-people

--- include/navbar.php
+++ include/navbar.php
@@ -7,15 +7,15 @@ echo "</style>";
 
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container-fluid">
-        <a class="navbar-brand mx-auto my-auto" href="/Project-trip"><img src="./public/svg/Logo.svg" alt=""></a>
-        <button class="navbar-toggler mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand mx-4 my-auto" href="/Project-trip"><img src="./public/svg/Logo.svg" alt=""></a>
+        <button class="navbar-toggler mx-4 py-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="bg-none"><img src="./public/svg/Picto-menu-burger.svg" alt=""></span>
         </button>
         <div class="collapse navbar-collapse mt-2" id="navbarTogglerDemo01">
 
             <ul class="navbar-nav me-auto mb-2 ml-1 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Le concept</a>
+                    <a class="nav-link" href="./views/concept.php">Le concept</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Destinations</a>
---
+++ views/concept.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/css/navbar.css">
+    <link rel="stylesheet" href="./public/css/footer.css">
+    <link rel="stylesheet" href="./public/css/page-accueil.css">
+    <link rel="shortcut icon" type="image/png" href="./public/svg/pointer.svg" />
+    <title>Culin'Air - Le concept</title>
+</head>
+
+<body>
+
+    <?php
+    include("./include/navbar.php");
+    ?>
+
+
+
+    <?php
+    include("./include/footer.php");
+    ?>
+
+</body>
+
+</html>
