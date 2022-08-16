d599ae5a8d414e7eab88a00bcf69c438f7e2ad29
DamienL <damien.laneelle33@gmail.com>
2022-08-16 08:43:25 +0200

    test 2

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<<<<<<< HEAD
+<?php 
+$page = $_GET['page'] ?? '';
+switch ($page){
+    case '/':
+        require __DIR__ . './projet/front/html/page-accueil.php';
+        break;    
+    case '':
+        require __DIR__ . './projet/front/html/page-accueil.php';
+        break;
+    //pour ajouter une page : 
+    // case 'nom de la page'
+    // require __DIR__ . '/projet/front/html/page-nom-de-la-page.php';
+    // pour les href : project-trip/nom-de-la-page
+    case 'dest':
+        require __DIR__ . '/projet/front/html/destinationsFrame.php';
+        break;
+    case 'connexion':
+        require __DIR__ . './projet/front/html/connexion.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/projet/front/html/page-404.php';
+        break;
+
+}
+?>
+=======
 <?php
 $page = $_GET['page'] ?? '';
 switch ($page) {
@@ -28,3 +56,4 @@ switch ($page) {
         require __DIR__ . './views/page-404.php';
         break;
 }
+>>>>>>> main
