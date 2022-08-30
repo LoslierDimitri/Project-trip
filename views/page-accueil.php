ee0fe7c5da8239b6d7165804b8382bd4a2266dcd
DamienL <damien.laneelle33@gmail.com>
2022-08-30 14:21:59 +0200

    include slideshow

--- views/page-accueil.php
+++ views/page-accueil.php
@@ -50,104 +50,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
     <?php
-        include("./include/regions_search.php")
+    include("./include/regions_search.php")
     ?>
 
-
-
-    <!-- 3D Slideshow Section -->
-    <section class="slideshow">
-        <div class="container">
-            <div class="entire-content">
-                <div class="content-carrousel">
-                    <figure class="d-flex align-items-center">
-                        <img src="./public/svg/Logo-simple.svg" alt="">
-                    </figure>
-                    <figure class="title d-flex align-items-center text-center w-100">
-                        <h2>Vous propose</h2>
-                    </figure>
-                    <figure class="shadow"><img src="./public/jpg/Bretagne.jpg" />
-                        <p>Dégustez la Bretagne</p>
-                    </figure>
-                    <figure class="shadow"><img src="./public/jpg/provence.jpg" />
-                        <p>Goûtez la Provence</p>
-                    </figure>
-                    <figure class="shadow"><img src="./public/jpg/Reunion.jpg" />
-                        <p>Savourez la Réunion</p>
-                    </figure>
-                    <figure class="shadow"><img src="./public/jpg/pays-basque.jpg" />
-                        <p>Croquez le Pays-Basque</p>
-                    </figure>
-                    <figure class="shadow"><img src="./public/jpg/Corse.jpg" />
-                        <p>Dévorez la Corse</p>
-                    </figure>
-                    <figure class="shadow"><img src="./public/jpg/Alsace.jpg" />
-                        <p>Régalez-vous de l'Alsace</p>
-                    </figure>
-                    <figure class="shadow guadeloupe"><img src="./public/jpg/Guadeloupe.jpg" />
-                        <p>Mordez la Guadeloupe<br> à pleines dents</p>
-                    </figure>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section>
-        <div class="advantages mb-4">
-            <div class="title mt-5 mb-5 d-flex">
-                <h2>Pourquoi partir avec</h2>
-                <img src="./public/svg/Logo-simple.svg" class="mx-3 pb-2" alt="">
-                <h2>?</h2>
-            </div>
-            <div class="contents">
-                <div class="row w-100">
-                    <div class="col-md-12 col-lg-3 mt-5 mb-3 mx-auto">
-                        <div class="card mx-auto" style="width: 18rem; background-color: transparent; border: none;">
-                            <img class="card-img-top mx-auto" src="./public/svg/Sur-mesure.svg" alt="Card image cap">
-                            <div class="card-body">
-                                <p class="card-text text-center text-white">Des voyages culinaires
-                                    sur mesure rien que
-                                    pour vous</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12 col-lg-3 mt-5 mb-3 mx-auto">
-                        <div class="card mx-auto" style="width: 18rem; background-color: transparent; border: none;">
-                            <img class="card-img-top mx-auto" src="./public/svg/Ecoresponsable.svg" alt="Card image cap">
-                            <div class="card-body">
-                                <p class="card-text text-center text-white">Nous privilégions les
-                                    voyages dans l’héxagone
-                                    pour une emprunte plus
-                                    écoresponsable</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12 col-lg-3 mt-5 mb-3 mx-auto">
-                        <div class="card mx-auto" style="width: 18rem; background-color: transparent; border: none;">
-                            <img class="card-img-top mx-auto" src="./public/svg/Meilleures-notes.svg" alt="Card image cap">
-                            <div class="card-body">
-                                <p class="card-text text-center text-white">Nous sélectionnons les
-                                    logements et restaurants
-                                    les mieux notés pour vous
-                                    garantir le meilleur</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12 col-lg-3 mt-5 mb-3 mx-auto">
-                        <div class="card mx-auto" style="width: 18rem; background-color: transparent; border: none;">
-                            <img class="card-img-top mx-auto" src="./public/svg/francophonie.svg" alt="Card image cap">
-                            <div class="card-body">
-                                <p class="card-text text-center text-white">Des trips francophones
-                                    pour plus de confort</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </section>
-
-
-
+    <?php
+    include("./include/slideshow.php")
+    ?>
 
     <?php
     include("./include/footer.php");
