b9c7695b59dbea0740b78bc4ed5d875fc7e9f781
DamienL <damien.laneelle33@gmail.com>
2022-08-31 16:26:16 +0200

    map dom-tom in progress

--- public/js/script.js
+++ public/js/script.js
@@ -10,6 +10,9 @@ For more information, please look at this pen's infos or ask me directly on Twit
 // http://www.creativejuiz.fr/blog/tutoriels/jquery-effet-smooth-scroll-defilement-fluide
 
 // pointing from the svg object to fragments in the HTML doc.
+
+// France map functions start
+
 $("#arrondissements a").click(function () {
   if ($(this).attr("xlink:href").substring(0, 1) == "#") {
     var the_id = $(this).attr("xlink:href");
@@ -39,25 +42,6 @@ function test(city_name) {
   alert("test " + city_name);
 }
 
-// function display_panel(region_name) {
-//     let zone;
-
-//     for (let i = 0; i < 13; i++) {
-//         zone = document.getElementById("region_name_" + (i + 1) + "");
-//         zone.setAttribute("fill", "#ffffff")
-//     }
-
-//     zone = document.getElementById(region_name);
-//     zone.setAttribute("fill", "#0FABDF")
-// }
-
-// for (let i = 0; i < 13; i++) {
-//     panel = document.getElementById('region_panel_' + (i + 1) + "");
-
-//     //panel = document.getElementById("panel");
-//     panel.classList.add("hidden_item");
-// }
-
 function display_panel(region_name, panel_name, details) {
   console.log(region_name);
   console.log(panel_name);
@@ -77,23 +61,18 @@ function display_panel(region_name, panel_name, details) {
 
   for (let i = 0; i < 13; i++) {
     panel = document.getElementById("region_panel_" + (i + 1) + "");
-
-    //panel = document.getElementById("panel");
     panel.classList.add("hidden_item");
   }
 
   panel = document.getElementById(panel_name);
   panel.classList.remove("hidden_item");
 
-  // panel_details = document.getElementById(details);
-  // panel_details.classList.add("show_item");
-
   panel_details = document.getElementById(details);
   panel_details.classList.add("show_item");
-
-  //   if (region_name == "region_name_1") {
-  //   }
-  //   if (region_name == "region_name_2") {
-  //     panel.innerHTML = panel_region_aquitaine;
-  //   }
 }
+
+//France map functions end
+
+//dom-tom map functions start
+
+//dom-tom map functions end
