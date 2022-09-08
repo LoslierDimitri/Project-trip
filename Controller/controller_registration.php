197532df8862bde43fe7cd520cbc136a6752f08e
LOSLIER Dimitri <loslierdimitri@gmail.com>
2022-09-08 09:23:11 +0200

    bug correction

--- Controller/controller_registration.php
+++ Controller/controller_registration.php
@@ -49,9 +49,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $password_after_hash = password_hash($password_before_hash, PASSWORD_DEFAULT);
 
             $database->insert_user($type, $inscription_name, $inscription_prenom, $inscription_age, $inscription_sexe, $inscription_pseudo, $password_after_hash, $inscription_email, $inscription_telephone, $inscription_pays, $inscription_adresse);
-            
-            $_SESSION["type"] = $database->get_user_information("type", "nom", $_POST["inscription_name"]);
-            $_SESSION["id"] = $database->get_user_information("id", "nom", $_POST["inscription_name"]);
+
+            $_SESSION["type"] = $database->get_user_information("type", "nom", $_POST["inscription_name"])[0]["type"];
+            $_SESSION["id"] = $database->get_user_information("id", "nom", $_POST["inscription_name"])[0]["name"];
             $_SESSION["pseudo"] = $database->get_user_information("pseudo", "pseudo", $_POST["inscription_pseudo"])[0]["pseudo"];
 
             echo ("<script>location.href = '/Project-trip/Controller/controller_main.php';</script>");
